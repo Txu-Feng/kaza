4de8803e9d7c1804842c93776f0a50cfcd2d6f2b
Txu-Feng LY <txu.feng.ly@gmail.com>
2023-09-09 13:53:15 +0200

    suppression de dépendance et commentaire inutiles

--- src/components/Carousel/Slider.jsx
+++ src/components/Carousel/Slider.jsx
@@ -5,15 +5,12 @@ import ArrowRight from '../../assets/arrow_right.png';
 
 export default function Slider({ pictures, title }) {
 
-  // Define a state variable 'currentIndex' to track the index of the currently displayed picture
   const [currentIndex, setCurrentIndex] = useState(0);
 
-// Function to move to the previous picture in the 'pictures' array
   const slidePrevious = () => {
     setCurrentIndex((prevIndex) => (prevIndex === 0 ? pictures.length - 1 : prevIndex - 1));
   };
 
-// Function to move to the next picture in the 'pictures' array
   const slideNext = () => {
     setCurrentIndex((nextIndex) => (nextIndex === pictures.length - 1 ? 0 : nextIndex + 1));
   };
